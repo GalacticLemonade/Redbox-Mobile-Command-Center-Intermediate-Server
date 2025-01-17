a62f75b935a07d77601e8a7f56fdb3a75b6aef55
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-16 17:32:06 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Redbox_Mobile_Command_Center_Intermediate_Server {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
