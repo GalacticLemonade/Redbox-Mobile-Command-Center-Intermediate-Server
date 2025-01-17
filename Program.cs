1ba62b2fd5d8ae5b32a2dcca904937b3cf28d63f
GalacticLemonade <aidenmoore1820@gmail.com>
2025-01-16 18:55:56 -0700

    sweet

--- Program.cs
+++ Program.cs
@@ -1,13 +1,30 @@
 ﻿namespace Redbox_Mobile_Command_Center_Intermediate_Server {
-    internal class Program {
+    class Program {
+        static TCPServer server;
+        static TCPClient client;
+
         static void Main(string[] args) {
-            TCPServer server = new TCPServer("0.0.0.0", 11500);
+            server = new TCPServer("0.0.0.0", 11500);
             server.Start();
 
-
+            //StartIP();
 
             // Prevent closing of application
             while (true) { }
+        }
+
+        public static async void StartIP() {
+            client = new TCPClient();
+            await client.ConnectAsync("192.168.1.123", 11600);
+        }
+
+        public async static Task<string> OnServerIncomingData(string message) {
+
+            //await client.SendMessageAsync(message);
+            //string response = await client.ReceiveMessageAsync();
+            //return response;
+
+            return String.Empty;
         }
     }
 }
